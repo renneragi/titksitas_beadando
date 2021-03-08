85399c302cf4389f3de9ed8bce8eecf5762eb191
Agnes Renner <agnes.renner.chilisoft@gmail.com>
2021-03-08 23:50:54 +0100

    szóköz kivétele a dekódolásból

--- Titkositas_beadando/src/Decoder.java
+++ Titkositas_beadando/src/Decoder.java
@@ -10,39 +10,41 @@ public class Decoder {
 	private String input;
 	private String fileName;
 	private char[] decodedData;
-		
-	
+
+
 	public Decoder(String fileName) {
 		super();
 		this.fileName = fileName;
 	}
 
 
-	void readData() throws FileNotFoundException, IOException {      
+	void readData() throws FileNotFoundException, IOException {
           this.input = new String(Files.readAllBytes(Paths.get(this.fileName)), StandardCharsets.UTF_8);
 	}
-	
+
 	void decoeData() {
 		char[] dataToEncode = this.input.toCharArray();
 		for (int i=0;i < dataToEncode.length; i++) {
-			dataToEncode[i]--;
-		}
+            if(dataToEncode[i] != ' '){
+                dataToEncode[i]--;
+            }
+					}
 		this.decodedData = dataToEncode;
 		this.writeData();
 	}
-	
-	void writeData(){      
+
+	void writeData(){
         PrintWriter outFile;
 		try {
 			outFile = new PrintWriter(new FileWriter("./target/decodedText.txt"));
 			outFile.write("dekódólt szöveg: ");
 			outFile.write(this.decodedData);
 			outFile.close();
-		
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}    
+		}
 	}
 
 }
